bca6ae83208020b22144b129a36d5d3234641a9e
Shahidkhan S <126046884+shahidkhan169@users.noreply.github.com>
2024-08-20 03:19:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ const app = express();
 // Middleware
 app.use(express.json());
 const corsOptions = {
-  origin: 'https://loot-bank-frontend.vercel.app', // Replace with your React app's URL
+  origin: 'https://loot-bank-frontend.vercel.app',
+  methods:['GET','POST','PUT','DELETE'],// Replace with your React app's URL
   credentials: true,
 };
 app.use(cors(corsOptions));
