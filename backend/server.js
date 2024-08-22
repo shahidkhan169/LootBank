73cbe278e0da813319507dac88c9656da6505897
shahidkhan169 <shahid1692004@gmail.com>
2024-08-22 21:05:51 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -4,25 +4,22 @@ const MongoStore = require("connect-mongo");
 const bank = require("./router/bank");
 const cors = require("cors");
 const helmet = require("helmet");
-const cookieParser = require("cookie-parser");
+require('dotenv').config();
 
 const app = express();
-require('dotenv').config();
-app.use(cors());
+
 app.use(cors({
-  origin: '*', // Ensure this is correct
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: ["https://loot-bank-frontend.vercel.app"], // Ensure this is correct
+  methods: ['GET', 'POST'],
   credentials: true
 }));
 
 app.use(helmet());
 app.use(express.json());
-app.use(cookieParser());
 
 
 // MongoDB connection URL
-const mongoUrl = process.env.MONGO_URL;
+const mongoUrl = "mongodb+srv://shahid1692004:rbaPyVM1wWINI3hz@lootbank.wlmtn.mongodb.net/?retryWrites=true&w=majority&appName=LootBank";
 
 
 // Connect to MongoDB
@@ -30,11 +27,8 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB:", err.message));
 
-
-// Routes
 app.use(bank);
 
-// Start the server
 app.listen(3000,()=>{
   console.log("server running at port 3000");
 }
