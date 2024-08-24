9948ac3b912ba61bc46253dfbe716cd60ff396e0
shahidkhan169 <shahid1692004@gmail.com>
2024-08-24 10:40:42 +0530

    final

--- backend/server.js
+++ backend/server.js
@@ -8,23 +8,24 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors(
-  {
-    origin:["http://localhost:5173"],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  }
-));
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
 app.use(helmet());
 app.use(express.json());
-mongoose.connect("mongodb+srv://vibeeshn21aid:yVoFz5rMYL5wbLEv@userdata.vrirq.mongodb.net/") 
+
+mongoose.connect("mongodb+srv://shahid1692004:bank1234@lootbank.wlmtn.mongodb.net/LootBank", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB:", err.message));
 
 app.use(bank);
 
-app.listen(3000,()=>{
-  console.log("server running at port 3000");
-}
-)
-
+app.listen(3000, () => {
+  console.log("Server running at port 3000");
+});
