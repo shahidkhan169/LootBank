9479bb86583d255b02a63a5e76ab89fdb1b8f617
shahidkhan169 <shahid1692004@gmail.com>
2024-08-24 11:19:08 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["https://loot-bank-frontend.vercel.app"],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
