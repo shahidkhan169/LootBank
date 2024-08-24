0ead495304feb76ed2390c3669c49d45c7c99fc0
shahidkhan169 <shahid1692004@gmail.com>
2024-08-24 13:10:04 +0530

    ok

--- frontend/LootBank/src/main.jsx
+++ frontend/LootBank/src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom'; // Import BrowserRouter
-import App from './App.jsx';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router> {/* Wrap App with BrowserRouter */}
-      <App />
-    </Router>
+    <App />
   </StrictMode>,
-);
+)
